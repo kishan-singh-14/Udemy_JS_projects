19628c487d7cae64842a19a8f1fdc8685ddeb0cc
kishan-singh-14 <kishansingh180.ks@gmail.com>
2022-08-09 19:11:49 +0530

    Challenge 2

--- Section_2/script.js
+++ Section_2/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const calculateAvg = (score1, score2, score3) => (score1 + score2 + score3)/3;
 
 const checkWinner = function(avgDolphins, avgKoalas) {
@@ -18,3 +18,18 @@ const avgDolphins1 = calculateAvg(84,54,41);
 const avgKoalas1 = calculateAvg(23,34,27);
 checkWinner(avgDolphins, avgKoalas);
 checkWinner(avgDolphins1, avgKoalas1);
+*/
+
+const calcTip = function (billAmount) {
+    const tip = (billAmount>=50 && billAmount<=300)? billAmount * 0.15 : billAmount * 0.20;
+    return tip;
+}
+
+const bills = [125,555,44];
+const totalAmount = [];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+for (let i=0; i<bills.length; i++) {
+    totalAmount[i] = bills[i] + tips[i];
+}
+console.log('Tips are ' + tips);
+console.log('Total amount is ' + totalAmount);
