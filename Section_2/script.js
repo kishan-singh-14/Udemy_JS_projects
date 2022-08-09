87cb78050de585d9bdf2af1134be8d5863473936
kishan-singh-14 <kishansingh180.ks@gmail.com>
2022-08-09 19:50:12 +0530

    Challenge 3

--- Section_2/script.js
+++ Section_2/script.js
@@ -18,8 +18,6 @@ const avgDolphins1 = calculateAvg(84,54,41);
 const avgKoalas1 = calculateAvg(23,34,27);
 checkWinner(avgDolphins, avgKoalas);
 checkWinner(avgDolphins1, avgKoalas1);
-*/
-
 const calcTip = function (billAmount) {
     const tip = (billAmount>=50 && billAmount<=300)? billAmount * 0.15 : billAmount * 0.20;
     return tip;
@@ -33,3 +31,32 @@ for (let i=0; i<bills.length; i++) {
 }
 console.log('Tips are ' + tips);
 console.log('Total amount is ' + totalAmount);
+*/
+
+const mark = {
+    name: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+
+    calcBMI : function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+}
+
+const john = {
+    name: 'John Smith',
+    mass: 92,
+    height: 1.95,
+
+    calcBMI : function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+}
+
+console.log(`${mark.name} has a bmi of ${mark.calcBMI()}.`);
+console.log(`${john.name} has a bmi of ${john.calcBMI()}.`);
+if (mark.bmi > john.bmi) console.log(`${mark.name}'s BMI(${mark.bmi}) is greater than ${john.name}'s BMI(${john.bmi}).`);
+else if (mark.bmi < john.bmi) console.log(`${john.name}'s BMI(${john.bmi}) is greater than ${mark.name}'s BMI(${mark.bmi}).`);
+else console.log("Both have the same BMI.");
