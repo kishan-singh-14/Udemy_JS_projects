33a3e16c418820ec39571e021d02809d85b6222d
mohanish_bharambe <mohanish.bharambe@zeuslearning.com>
2022-08-09 12:30:06 +0530

    First Commit

---
+++ Section_1/script.js
@@ -0,0 +1,11 @@
+const marksWeight = 78; 
+const johnsWeight = 95; 
+const marksHeight = 1.69; 
+const johnsHeight = 1.88; 
+
+let markBMI = marksWeight / (marksHeight * marksHeight);
+let johnBMI = johnsWeight / johnsHeight ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+document.getElementById('challenge1').innerText = String(markBMI) + johnBMI + markHigherBMI;
+console.log(markBMI, johnBMI, markHigherBMI);
