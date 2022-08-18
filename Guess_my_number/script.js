c931e65c01ac7716171c823e85f42734b14e8de5
kishan-singh-14 <kishansingh180.ks@gmail.com>
2022-08-18 19:03:06 +0530

    DRY code

--- Guess_my_number/script.js
+++ Guess_my_number/script.js
@@ -5,33 +5,19 @@ let score = 20;
 let highScore = 0;
 console.log(secretAnswer);
 
+const displayMessage = function (msg) {
+    document.querySelector('.message').textContent = msg ;
+}
+
 document.querySelector('.check').addEventListener('click',function () {
-    const guess = document.querySelector('.guess').value;
+    const guess = Number(document.querySelector('.guess').value);
     console.log(guess, secretAnswer);
 
     if (!guess) {
-        document.querySelector('.message').textContent = 'No answer!!' ;    
-    } else if (secretAnswer < guess) {
-        // Too High
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too High \u2191' ;
-            score--;
-        } else  {
-            document.querySelector('.message').textContent = 'Sorry you lost the game!' ;
-            score = 0;
-        }
-        
-    } else if (secretAnswer >  guess) {
-        // Too low
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too Low \u2193' ;
-            score--;
-        } else  {
-            document.querySelector('.message').textContent = 'Sorry you lost the game!' ;
-            score = 0;
-        }
-    } else {
-        document.querySelector('.message').textContent = 'Correct Answer!  !';
+        displayMessage('No answer!!');    
+    } else if (guess === secretAnswer) {
+        console.log("equal");
+        displayMessage('Correct Answer!!');
         document.querySelector('.number').textContent = guess;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
@@ -39,7 +25,16 @@ document.querySelector('.check').addEventListener('click',function () {
             highScore = score;
             document.querySelector('.highscore').textContent = highScore;
         }
-    }
+    } else if (secretAnswer !== guess) {
+        console.log("not equal");
+        if (score > 1) {
+            displayMessage(secretAnswer > guess ? 'Too Low \u2193' : 'Too High \u2191');
+            score--;
+        } else  {
+            displayMessage('Sorry you lost the game!');
+            score = 0;
+        }
+    } 
     document.querySelector('.score').textContent = score;
 });
 
@@ -47,7 +42,7 @@ document.querySelector('.again').addEventListener('click', function() {
     score = 20;
     document.querySelector('.number').textContent = '?';
     document.querySelector('.score').textContent = score;
-    document.querySelector('.message').textContent = 'Start guessing...';
+    displayMessage('Start guessing...');
     document.querySelector('.guess').value = "";
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15rem';
